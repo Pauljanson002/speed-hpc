a1846ec41eefaab840f33c40b36778e08a33c2da
Paul Janson <pauljanson002@gmail.com>
2024-10-04 18:46:30 -0400

    Update start_jupyterlab.sh

--- src/jupyterlabs/start_jupyterlab.sh
+++ src/jupyterlabs/start_jupyterlab.sh
@@ -1,3 +1,11 @@
-# Start either a part of interactive or batch sesions
-# Hardcoded assumption: /speed-scratch/$USER/speed-hpc/src/jupyterlabs
-srun --mem=16G --gpus=1 -p pt --mail-type=ALL /speed-scratch/$USER/speed-hpc/src/jupyterlabs/run_jupyterlab.sh
+#!/bin/tcsh
+
+# Check if a memory argument was provided, otherwise default to 16G
+set MEMORY = "$1"
+if ("$MEMORY" == "") then
+    set MEMORY = "16G"
+endif
+
+# Submit the job with the specified or default memory
+srun --mem=$MEMORY --gpus=1 -p pt --mail-type=ALL /speed-scratch/$USER/speed-hpc/src/jupyterlabs/run_jupyterlab.sh
+
